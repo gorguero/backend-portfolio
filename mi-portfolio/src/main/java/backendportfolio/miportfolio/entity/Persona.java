7c74affa50cc0589908d7028abfec3d50d8e0797
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-08-28 16:55:45 -0300

    Creando entidades
    Creando entidades de la base de datos y agregando dependencia

---
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/entity/Persona.java
@@ -0,0 +1,68 @@
+
+package backendportfolio.miportfolio.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Persona implements Serializable{
+    
+    @Id
+    @GeneratedValue (strategy=GenerationType.AUTO)
+    @Column(nullable=false, updatable=false)
+    private Long idPersona;
+    
+
+    private String nombre;
+    private String apellido;
+    private String presentacionUno;
+    private String presentacionDos;
+    private String telefono;
+    private String email;
+    private String ubicacion;
+    private String link_perfil;   
+    private String perfilLaboral;
+
+    
+//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEducacion") //LAZY -> Hace una consulta especifica de si queremos traer todo lo del usuario
+//    private List<Educacion> educacionList; 
+//    
+//    @OneToMany
+//    private List<ExperienciaLaboral> expLaboralList;
+//    
+//    @OneToMany
+//    private List<Softskill> skillList;
+//    
+//    @OneToMany
+//    private List<Proyecto> proyectoList;
+//    
+//    @OneToMany
+//    private List<Tecnologia> tecnologiaList;
+
+    public Persona() {
+    }
+
+    public Persona(Long idPersona, String nombre, String apellido, String presentacionUno, String presentacionDos, String telefono, String email, String ubicacion, String link_perfil, String perfilLaboral) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.presentacionUno = presentacionUno;
+        this.presentacionDos = presentacionDos;
+        this.telefono = telefono;
+        this.email = email;
+        this.ubicacion = ubicacion;
+        this.link_perfil = link_perfil;
+        this.perfilLaboral = perfilLaboral;
+    }
+
+    
+}
---
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/entity/Proyecto.java
@@ -0,0 +1,35 @@
+
+package backendportfolio.miportfolio.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Proyecto {
+    
+    @Id
+    @GeneratedValue (strategy=GenerationType.AUTO)
+    private Long idProyecto;
+    
+    private String titulo;
+    private String descripcion;
+    private String link_portada;
+    private String link_proyecto;
+
+    public Proyecto() {
+    }
+
+    public Proyecto(Long idProyecto, String titulo, String descripcion, String link_portada, String link_proyecto) {
+        this.idProyecto = idProyecto;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.link_portada = link_portada;
+        this.link_proyecto = link_proyecto;
+    }
+    
+}
