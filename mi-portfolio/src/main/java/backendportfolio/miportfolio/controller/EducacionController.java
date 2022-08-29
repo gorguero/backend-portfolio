77b29651e5321e02a94021722ef6c8edeef5bd61
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-08-29 11:22:51 -0300

    Cargando los controllers para cada entidad

---
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/EducacionController.java
@@ -0,0 +1,70 @@
+
+package backendportfolio.miportfolio.controller;
+
+import backendportfolio.miportfolio.entity.Educacion;
+import backendportfolio.miportfolio.interfaces.IEducacionService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class EducacionController {
+    
+    @Autowired
+    private IEducacionService educacionService;
+    
+    @GetMapping ("/educacion/obtener")
+    @ResponseBody
+    public List<Educacion> getEducacion(){
+        return educacionService.getEducacion();
+    }
+    
+    @PostMapping ("/educacion/crear")
+    public String agregarEducacion(@RequestBody Educacion educacion){
+        educacionService.saveEducacion(educacion);
+        return "Se ha creado correctamente";
+    }
+    
+    @DeleteMapping ("/educacion/borrar/{id}")
+    public String eliminarEducacion(@PathVariable Long id){
+        educacionService.deleteEducacion(id);
+        return "Se ha eliminado correctamente.";
+    }
+    
+    @PutMapping ("/educacion/editar/{id}")
+    public Educacion editarEducacion(@PathVariable Long id,
+                                    @RequestParam ("nombreInsti") String nuevoNombreInsti,
+                                    @RequestParam ("link_logo") String nuevoLogo,
+                                    @RequestParam ("titulo") String nuevoTitulo,
+                                    @RequestParam ("fechaInicio") String nuevoFechaInicio,
+                                    @RequestParam ("fechaFinal") String nuevoFechaFinal){
+        
+        Educacion educacion = educacionService.findEducacion(id);
+        
+        educacion.setNombreInsti(nuevoNombreInsti);
+        educacion.setLink_logo(nuevoLogo);
+        educacion.setTitulo(nuevoTitulo);
+        educacion.setFechaInicio(nuevoFechaInicio);
+        educacion.setFechaFinal(nuevoFechaFinal);
+        
+        educacionService.saveEducacion(educacion);
+        
+        return educacion;
+    }
+    
+    @GetMapping ("/educacion/encontrar/{id}")
+    public Educacion findEducacion(@PathVariable Long id){
+        return educacionService.findEducacion(id);
+    }
+    
+}
---
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/ExperienciaLaboralController.java
@@ -0,0 +1,71 @@
+
+package backendportfolio.miportfolio.controller;
+
+import backendportfolio.miportfolio.entity.ExperienciaLaboral;
+import backendportfolio.miportfolio.interfaces.IExperienciaLaboralService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class ExperienciaLaboralController {
+    
+    @Autowired
+    private IExperienciaLaboralService expLaboralService;
+    
+    @GetMapping ("/expLaboral/obtener")
+    @ResponseBody
+    public List<ExperienciaLaboral> getExperienciaLaboral(){
+        return expLaboralService.getExperienciaLaboral();
+    }
+    
+    @PostMapping ("/expLaboral/crear")
+    public String agregarExperienciaLaboral(@RequestBody ExperienciaLaboral expLaboral){
+        expLaboralService.saveExperienciaLaboral(expLaboral);
+        return "Se ha creado correctamente";
+    }
+    
+    @DeleteMapping ("/expLaboral/borrar/{id}")
+    public String eliminarExperienciaLaboral(@PathVariable Long id){
+        expLaboralService.deleteExperienciaLaboral(id);
+        return "Se ha eliminado correctamente";
+    }
+    
+    @PutMapping ("/expLaboral/editar/{id}")
+    public ExperienciaLaboral editarExpLaboral(@PathVariable Long id,
+                                             @RequestParam ("nombreEmpresa") String nuevoNombreEmpresa,
+                                             @RequestParam ("link_logo") String nuevoLogo,
+                                             @RequestParam ("nombrePuesto") String nuevoNombrePuesto,
+                                             @RequestParam ("fechaIngreso") String nuevoFechaIngreso,
+                                             @RequestParam ("fechaFin") String nuevoFechaFin){
+        
+        ExperienciaLaboral expLaboral = expLaboralService.findExperienciaLaboral(id);
+        
+        expLaboral.setNombreEmpresa(nuevoNombreEmpresa);
+        expLaboral.setLink_logo(nuevoLogo);
+        expLaboral.setNombrePuesto(nuevoNombrePuesto);
+        expLaboral.setFechaIngreso(nuevoFechaIngreso);
+        expLaboral.setFechaFin(nuevoFechaFin);
+        
+        expLaboralService.saveExperienciaLaboral(expLaboral);
+        
+        return expLaboral;
+        
+    }
+    
+    @GetMapping ("/expLaboral/encontrar/{id}")
+    public ExperienciaLaboral findEducacion(@PathVariable Long id){
+        return expLaboralService.findExperienciaLaboral(id);
+    }
+    
+}
