8427f4eb3307c9ab7f425356ae4f7f07b1c38813
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-09-26 19:58:12 -0300

    Ajuste final

--- mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/ExperienciaLaboralController.java
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/ExperienciaLaboralController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-miportfolio.web.app")
 public class ExperienciaLaboralController {
     
     @Autowired
--- mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/PersonaController.java
+++ mi-portfolio/src/main/java/backendportfolio/miportfolio/controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-miportfolio.web.app")
 public class PersonaController {
     
     @Autowired
